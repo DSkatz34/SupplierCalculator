5933cc3ccdf8d2c34b63b848ce75e8507dc9cd3b
DSkatz34 <DSkatz34@users.noreply.github.com>
2022-12-20 12:04:01 -0500

    Add project files.

---
+++ SupplierCalculator/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SupplierCalculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            using (var context = new Ubs_ProdEntities())
+            {
+                var salesTolals = context.p_CountAverageWeeklyBySupplier().ToList();
+                return View(salesTolals);
+            } 
+          
+        }
+        public ActionResult TotalBySupplier(int SupplierId)
+        {
+
+            using (var context = new Ubs_ProdEntities())
+            {
+                var salesTolals = context.p_CountAverageWeeklyBySupplier().Where(x=>x.Supplierid == SupplierId).ToList();
+                return View("Index",salesTolals);
+            }
+
+        }
+
+    }
+}
